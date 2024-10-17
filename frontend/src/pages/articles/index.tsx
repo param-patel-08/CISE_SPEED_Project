bf6c9e9d647e8f361caccc8cfb0b95af83f64a7a
Param Patel <param.patel01@gmail.com>
2024-10-17 20:44:04 +1300

    added comments to article index page

--- frontend/src/pages/articles/index.tsx
+++ frontend/src/pages/articles/index.tsx
@@ -1,8 +1,12 @@
-import React from "react"
-import ArticlesView from "../../components/articles/ArticlesView"
+// Import React and the ArticlesView component from the components folder
+import React from "react";
+import ArticlesView from "../../components/articles/ArticlesView";
 
+// Define a functional component called Articles
 export default function Articles() {
+  // Render the ArticlesView component
   return (
     <ArticlesView></ArticlesView>
-  )
+    // Alternatively, you could use the shorthand <ArticlesView /> for self-closing tags
+  );
 }
